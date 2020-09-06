b0892b328fedaaad45a3da2394a397cdfdafa9d4
CaryLaudadio <carylaudadio@rogers.com>
2020-09-06 19:14:57 -0400

    AddingProject

---
+++ Museum/Assets/InstantiateObject.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InstantiateObject : MonoBehaviour
+{
+    public GameObject prefab;
+
+    public void InstantiatePrefab()
+    {
+        Instantiate(prefab, transform.position, transform.rotation, null);
+    }
+}
