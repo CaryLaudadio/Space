1825223cd5567f6326f0eaa775424e4abf83c6a5
CaryLaudadio <carylaudadio@rogers.com>
2020-09-20 08:33:30 -0400

    updtes

---
+++ Museum/Assets/Scripts/CameraMove.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMove : MonoBehaviour
+{
+    public Transform Cube, Sphere, Cylinder;
+    Vector3 OrigCubePos;
+    Vector3 OrigCubeRot;
+    public float CubeSpeed = 3f;
+    public float CubeRotSpeed = .125f;
+
+    Vector3 OrigCameraPos;
+    Vector3 OrigCameraRot;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        OrigCubePos = Cube.position;
+        OrigCameraPos = this.transform.position;
+        OrigCubeRot = Cube.rotation.eulerAngles;
+        OrigCameraRot = this.transform.rotation.eulerAngles;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        Vector3 DeltaCubePos;
+        DeltaCubePos = ( Cube.position - OrigCubePos ) * CubeSpeed;
+        this.transform.position = OrigCameraPos + DeltaCubePos;
+
+        Vector3 DeltaCubeRot;
+        DeltaCubeRot = (Cube.rotation.eulerAngles - OrigCubeRot) * CubeRotSpeed;
+        Quaternion newRotation;
+        newRotation = Quaternion.Euler(OrigCameraRot + DeltaCubeRot);
+        this.transform.rotation = newRotation;
+
+
+
+    }
+}
---
+++ Museum/Assets/Teleport.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleport : MonoBehaviour
+{
+    public Transform Destination;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        other.transform.position = Destination.position;
+    }
+
+}
